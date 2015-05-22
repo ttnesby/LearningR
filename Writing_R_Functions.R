1e09f86e0ec515744e2596d9dfa3b970ae2e3e58
ttnesby <ttnesby@gmail.com>
2015-05-23 01:48:35 +0200

    as.vector

--- Writing_R_Functions.R
+++ Writing_R_Functions.R
@@ -77,3 +77,5 @@ do.call("hello.fullname", args = list(first = "Torstein", last = "Nesby"))
 do.call(hello.fullname, args = list(first = "Torstein", last = "Nesby"))
 do.call(hello.last, args = list(fullNDF$firstN))
 
+as.vector(fullNDF$firstN)
+
