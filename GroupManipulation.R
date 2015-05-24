1b2c73fc2f94046d1511dc2e64c332c787a65925
ttnesby <ttnesby@gmail.com>
2015-05-24 19:42:48 +0200

    Group mani

--- GroupManipulation.R
+++ GroupManipulation.R
@@ -39,6 +39,22 @@ mapply(ttn.length2Lists, firstList, secondList)
 # Balancing act between *apply functions and plyr package
 # tapply, rapply, eapply, vapply, by - advantages over plyr
 
+# Baseball to Mathematica ----
+
+wlCSV <- function(dataSet, fname)
+{
+  homeDir <- "~/Rtmp"
+  if (!dir.exists(homeDir)) {
+    dir.create(homeDir)
+  }
+  write.csv(
+    dataSet, 
+    file = paste(paste(homeDir, fname, sep = "/"),"csv", sep = "."), 
+    fileEncoding = "UTF-8"
+  )
+}
+
+
 # aggregate ----
 
 require(ggplot2)
@@ -63,5 +79,31 @@ require(plyr)
 data(baseball)
 head(baseball)
 
+wlCSV(baseball, "baseball")
+
 # ddply - input data.frame - output - data.frame
 
+baseball$sf[baseball$year < 1954] <- 0 # players before 1954, sacrifice flies to be 0 instead of NA
+any(is.na(baseball$sf))                # checking for NA
+baseball$hbp[is.na(baseball$hbp)] <- 0 # converting NA to 0
+any(is.na(baseball$hbp))               # checking for NA
+
+baseball <- baseball[baseball$ab >= 50, ] # keep player with at least 50 at bats
+
+# calculate according to formula - adding a new column
+baseball$OBP <- with(baseball, (h + bb + hbp)/(ab + bb + hbp + sf)) # with - can use column names only
+
+tail(baseball)
+
+# To calculate the OBP for the entire career, need to sum all variables for numerator and denominator
+# use of ddply
+
+# help function for calc OBP for each player
+obp <- function(data)
+{
+  c(OBP = with(data, sum(h + bb + hbp)/sum(ab + bb + hbp + sf)))
+}
+
+careerOBP <- ddply(baseball, .variables = "id", .fun = obp) # id - each player's career
+careerOBP <- careerOBP[order(careerOBP$OBP, decreasing = TRUE), ] 
+head(careerOBP, 10)
